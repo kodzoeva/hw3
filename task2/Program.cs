a3b84145b0f01595fc4b318e06deb230c020abed
kodzoeva <repozit@mail.ru>
2023-02-07 21:24:53 +0300

    hwc3

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// // Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear ();
+int Coordinate(string coorName, string pointName)
+{
+    System.Console.WriteLine($"Введите координату {coorName} точки {pointName}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int х1 = Coordinate("х", "a");
+int y1 = Coordinate("y", "a");
+int z1 = Coordinate("z", "a");
+int х2 = Coordinate("х", "b");
+int y2 = Coordinate("y", "b");
+int z2 = Coordinate("z", "b");
+
+double Calculator(double х1, double х2, double у1, double у2, double z1, double z2)
+{
+    double action1 = Math.Pow (х2-х1, 2);
+    double action2 = Math.Pow(y2-y1, 2);
+    double action3 = Math.Pow(z2-z1, 2);
+    double result = Math.Sqrt(action1 + action2 + action3);
+    return result;
+}
+ 
+ double length = Calculator (х1, х2, y1, y2, z1, z2);
+ System.Console.WriteLine($"Длина отрезка равна > {length:F3}");
---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// //Напишите программу, которая принимает на вход |
+// пятизначное число и проверяет, является ли оно 
+// палиндромом. Не используйте строки
+
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Введеное число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Введенное число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - введите пятизначное число");
+}
+
+
